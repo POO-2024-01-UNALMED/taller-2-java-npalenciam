1341f93b671ad819c4b4dbf2b1cec8a337739638
npalenciam <npalenciam@unal.edu.co>
2024-02-20 03:04:40 -0500

    Metodo verificar integridad agregado
    Metodo para verificar la originalidad de los componentes se adjuntó en la clase Auto

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -7,7 +7,7 @@ public class Asiento {
 	
 	void cambiarColor (String color) {
 		if (color.equals("rojo") || color.equals("verde") || color.equals("amarillo") || color.equals("negro") || color.equals("blanco")) {
-		this.color = color;
+			this.color = color;
 		}
 	}
 }
--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -19,6 +19,17 @@ public class Auto {
 		return numeroAsientos;
 	}
 	
-	
-
+	String verificarIntegridad() {
+		if (this.registro == this.motor.registro) {
+			for (int i = 0; i < asientos.length ; i++) {
+				if (asientos[i] != null && asientos[i].registro != registro) {
+					return "Las piezas no son originales" ;
+				}
+			}
+			return "Auto original";
+		}
+		else {
+			return "Las piezas no son originales";
+		}
+	}
 }
